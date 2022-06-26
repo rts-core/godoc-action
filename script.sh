39fc0e6923d69798f5f51ff88740adf71e4b6c6b
Lucas Roe <lroe@realtechnologysolutions.io>
2022-06-25 22:12:08 -0400

    Action debugging
    - Trying to get doc gen actions working

--- script.sh
+++ script.sh
@@ -1,46 +1,28 @@
 #!/bin/bash
 
-# This code is originally from https://github.com/ktr0731/godoc-action which fell out of update.
-
 set -eo pipefail
 
+# Make sure background processes are killed on script leave
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+# find go mod and make sure we're using it's parent as our root.
 cd "$(dirname "$(find . -name 'go.mod' | head -n 1)")" || exit 1
 
-MODULE_ROOT="$(go list -m)"
-REPO_NAME="$(basename $(echo $GITHUB_REPOSITORY))"
-PR_NUMBER="$(echo $GITHUB_REF | sed 's#refs/pull/\(.*\)/.*#\1#')"
+# clean and make our docs dir
+rm -rf ${INPUT_HTML_DIR}
+mkdir -p ${INPUT_HTML_DIR}
 
-mkdir -p "$GOPATH/src/github.com/$GITHUB_REPOSITORY"
-cp -r * "$GOPATH/src/github.com/$GITHUB_REPOSITORY"
-(cd /tmp && godoc -http localhost:8080 &)
-
+# host godoc
+godoc -http=:8080 &
 for (( ; ; )); do
   sleep 0.5
-  if [[ $(curl -so /dev/null -w '%{http_code}' "http://localhost:8080/pkg/$MODULE_ROOT/") -eq 200 ]]; then
+  if [[ $(curl -so /dev/null -w '%{http_code}' "http://localhost:8080/pkg/") -eq 200 ]]; then
     break
   fi
 done
 
-git checkout origin/gh-pages || git checkout -b gh-pages
+# get our html
+cd ${INPUT_HTML_DIR}
+wget -m -k -q -erobots=off --no-host-directories --no-use-server-timestamps http://localhost:8080
 
-wget --quiet --mirror --show-progress --page-requisites --execute robots=off --no-parent "http://localhost:8080/pkg/$MODULE_ROOT/"
-
-rm -rf doc lib "$PR_NUMBER" # Delete previous documents.
-mv localhost:8080/* .
-rm -rf localhost:8080
-find pkg -type f -exec sed -i "s#/lib/godoc#/$REPO_NAME/lib/godoc#g" {} +
-
-git config --local user.email "action@github.com"
-git config --local user.name "GitHub Action"
-[ -d "$PR_NUMBER" ] || mkdir "$PR_NUMBER"
-mv pkg "$PR_NUMBER"
-git add "$PR_NUMBER" doc lib
-git commit -m "Update documentation"
-
-GODOC_URL="https://$(dirname $(echo $GITHUB_REPOSITORY)).github.io/$REPO_NAME/$PR_NUMBER/pkg/$MODULE_ROOT/index.html"
-
-if ! curl -sH "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$PR_NUMBER/comments" | grep '## GoDoc' > /dev/null; then
-  curl -sH "Authorization: token $GITHUB_TOKEN" \
-    -d '{ "body": "## GoDoc\n'"$GODOC_URL"'" }' \
-    "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$PR_NUMBER/comments"
-fi
+exit 0
